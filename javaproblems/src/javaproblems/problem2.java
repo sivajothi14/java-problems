f4881784d65cf81e1e70b03259d75b16a06e51e4
sivajothi14 <jothiengineer14@gmail.com>
2025-03-06 18:57:05 +0530

    commit 2

---
+++ javaproblems/src/javaproblems/problem2.java
@@ -0,0 +1,19 @@
+package javaproblems;
+import java.util.Scanner;
+
+public class problem2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		int num=sc.nextInt();
+		if(num%2==0) {
+			System.out.println("IT IS A EVEN NUMBER");
+		}else {
+			System.out.println("IT IS A ODD NUMBER");
+		}
+		
+
+	}
+
+}
