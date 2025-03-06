c39b7bf5ee0014aa2137567570d22a9adc8cf71e
sivajothi14 <jothiengineer14@gmail.com>
2025-03-06 19:02:40 +0530

    commit 3

---
+++ javaproblems/src/javaproblems/problem3.java
@@ -0,0 +1,15 @@
+package javaproblems;
+import java.util.Scanner;
+//Take name as input and print a greeting message for that particular name.
+public class problem3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a name");
+		String name=sc.nextLine();
+		System.out.println("HELLO"+ name);
+		// TODO Auto-generated method stub
+
+	}
+
+}
