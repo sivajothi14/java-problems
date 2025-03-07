cef7ccc23b18cc0a8a13d1f7f27d09c48ebb600b
sivajothi14 <jothiengineer14@gmail.com>
2025-03-07 21:22:18 +0530

    commit 7

---
+++ javaproblems/src/javaproblems/problem7.java
@@ -0,0 +1,15 @@
+package javaproblems;
+import java.util.Scanner;
+//take input as rupees and print output in dollars.
+
+public class problem7 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER RUPEES");
+		double rupees=sc.nextFloat();
+		double dollar=0.011;
+		System.out.println("THE DOLLAR IS:"+ 0.011*rupees);
+		
+		}
+
+}
