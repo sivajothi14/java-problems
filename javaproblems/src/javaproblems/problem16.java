486b9891e714583df7b2276ea1311733c0749792
SIVAJOTHI <jothiengineer14@gmail.com>
2025-03-08 19:03:15 +0530

    problem16.java

--- javaproblems/src/javaproblems/problem16.java
+++ javaproblems/src/javaproblems/problem16.java
@@ -1,23 +1,22 @@
 package javaproblems;
 import java.util.Scanner;
-//Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
 
-public class problem16 {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		// TODO Auto-generated method stub
-		System.out.println("enter the days of a month");
-		int n=sc.nextInt();
-			if(n%2==0) {
-				System.out.println("kunal is allowed to go outside");
-			}
-			for(int count=0;count<=n;count++) {
-				count=count/10;
-				System.out.println("number of digits:" + count);
-				
-		}
-			
+public class Problem16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // August has 31 days
+        int totalDays = 31;
+        int count = 0;
+        
+        for (int day = 1; day <= totalDays; day++) {
+            if (day % 2 == 0) {
+                count++;
+            }
+        }
+        
+        System.out.println("Kunal can go out on " + count + " days in August.");
+        
+        sc.close();
+    }
 }
-	}
-
